3c033dab2b419866608c5cfc99185552b15292b2
ajithbabu08 <ajithpersonaluse@gmail.com>
2023-09-26 14:53:53 +0530

    Commit

--- lib/storages in flutter/hive using hive adapter/screens/login_page.dart
+++ lib/storages in flutter/hive using hive adapter/screens/login_page.dart
@@ -1,15 +1,18 @@
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:practice/storages%20in%20flutter/hive%20using%20hive%20adapter/database/hivedb.dart';
 import 'package:practice/storages%20in%20flutter/hive%20using%20hive%20adapter/screens/register_page.dart';
 
 import '../models/user_model.dart';
+import 'home.dart';
 
 
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  // Hive.registerAdapter();
+  Hive.registerAdapter(UserAdapter());
   await Hive.openBox<User>('userData');
   runApp(MaterialApp(home: Login(),));
 }
@@ -45,7 +48,11 @@ class Login extends StatelessWidget {
             ),
           ),
 
-          ElevatedButton(onPressed: (){},
+          ElevatedButton(
+              onPressed: ()async{
+              final users=await HiveDb.instance.getUser();
+              checkUserExist(context,users);
+          },
               child: Text("Login")),
           TextButton(onPressed: (){
             Navigator.of(context).push(MaterialPageRoute(builder: (context)=>RegisterHive()));
@@ -55,4 +62,29 @@ class Login extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> checkUserExist(BuildContext context, List<User> users) async{
+    final lemail=email.text.trim();
+    final lpass=pass.text.trim();
+    bool userFound=false;
+    if(lemail !="" && lpass!=""){
+      await Future.forEach(users, (singleUser) {
+        if(lemail==singleUser.email && lpass==singleUser.password){
+          userFound=true;
+        }else{
+          userFound=false;
+        }
+      });
+      if(userFound==true){
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>HiveHome(email: lemail,)));
+      }else{
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Login failed, User not found")));
+      }
+    }else{
+
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Fiels must not  be empty")));
+        }
+
+  }
+
 }
