33b40fd10f11b1f3ba1c18ad085a8f0344514319
ajithbabu08 <ajithpersonaluse@gmail.com>
2023-10-05 22:23:54 +0530

    Commit

---
+++ lib/firebase_examplepgm/email_pass_auth/home_fir.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home_Fire extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
---
+++ lib/firebase_examplepgm/email_pass_auth/login_fir.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:practice/firebase_examplepgm/email_pass_auth/firebase_db.dart';
+
+import 'home_fir.dart';
+
+
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  User? user=FirebaseAuth.instance.currentUser;
+  runApp(MaterialApp(home: user==null? Login_fire() : Home_Fire(),));
+}
+
+
+class Login_fire extends StatefulWidget {
+
+
+  @override
+  State<Login_fire> createState() => _Login_fireState();
+}
+
+class _Login_fireState extends State<Login_fire> {
+
+  final username_controller=TextEditingController();
+  final passsword_controller=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login"),),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: const InputDecoration(border: OutlineInputBorder(),
+                  hintText: "Username"),
+              controller: username_controller,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: const InputDecoration(border: OutlineInputBorder(),
+                  hintText: "Password"),
+              controller: passsword_controller,
+            ),
+          ),
+          ElevatedButton(onPressed: (){
+            String email=username_controller.text.trim();
+            String pass=passsword_controller.text.trim();
+            FirebaseHelper().login(email:email, password:pass).then((result){
+              if(result==null){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Home_Fire()));
+              }else{
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result)));
+              }
+            });
+          },
+              child: const Text("Login")),
+          SizedBox(height: 15,),
+          TextButton(onPressed: (){}, child: const Text("Register Here"))
+        ],
+      ),
+    );
+  }
+}
